1ee173d99e8ebd55c5eea42abd434a61574e1523
Divi1729 <divibharadwaj97@gmail.com>
2017-07-09 12:37:57 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,20 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class EO
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int number;
+	    Scanner s=new scanner(System.in);
+	    System.out.println("number:");
+	    number=s.nextInt();
+		if(number%2==0)
+		{System.out.println("The number is even.");}
+		else if(number%2!=0)
+		{System.out.println("The number is odd");}
+		
+	}
+}
